a3483e2d522322d67626ee1a1bc2cb9124c0d0af
keshavgarg119 <kgarg2_be23@thapar.edu>
2025-07-01 12:47:51 +0530

    Time: 2 ms (13.10%) | Memory: 9 MB (20.37%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for (char ch : s) {
+            if (ch == '(' || ch == '{' || ch == '[') {
+                st.push(ch);
+            } else {
+                if (st.empty()) {
+                    return false;
+                }
+                char top = st.top();
+                if ((ch == ')' && top != '(') ||
+                    (ch == '}' && top != '{') ||
+                    (ch == ']' && top != '[')) {
+                    return false;
+                }
+                st.pop(); // Matching pair, so pop and continue
+            }
+        }
+        return st.empty(); // Must be empty if all brackets matched
+    }
+};
