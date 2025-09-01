56fbda33affc308cbcb69039b3cac0ef92642e8b
keshavgarg119 <kgarg2_be23@thapar.edu>
2025-09-01 14:29:12 +0530

    Time: 312 ms (52.34%) | Memory: 97.8 MB (81.29%) - LeetSync

---
+++ 1917-maximum-average-pass-ratio/maximum-average-pass-ratio.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    double maxAverageRatio(vector<vector<int>>& classes, int extraStudents) {
+        
+        auto gain = [](int pass, int total) {
+            return (double)(pass+1)/(total+1) - (double)(pass)/(total);
+        };
+
+        priority_queue<pair<double,pair<int,int>>>pq;
+
+        for(auto &c : classes) {
+            pq.push({gain(c[0],c[1]), {c[0],c[1]}});
+        }
+
+        while(extraStudents--) {
+            auto top = pq.top();
+            pq.pop();
+
+            int pass = top.second.first;
+            int total = top.second.second;
+            pass++;
+            total++;
+
+            pq.push({gain(pass,total),{pass,total}});
+        }
+
+        double sum = 0.0;
+
+        while (!pq.empty()) {
+            auto [gainVal, pr] = pq.top(); pq.pop();
+            sum += (double)pr.first / pr.second;
+        }
+        
+        return sum / classes.size();
+    }
+};
