366022e0dc486d6cee92654b2b9c3f158170d221
keshavgarg119 <kgarg2_be23@thapar.edu>
2025-09-04 12:15:57 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Linked List Group Reverse/linked-list-group-reverse.cpp
@@ -0,0 +1,58 @@
+/*
+class Node {
+  public:
+    int data;
+    Node* next;
+
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+*/
+class Solution {
+  public:
+    Node *reverseKGroup(Node *head, int k) {
+    if (head == nullptr) {
+        return head;
+    }
+
+    Node *curr = head;
+    Node *newHead = nullptr;
+    Node *tail = nullptr;
+
+    while (curr != nullptr) {
+        Node *groupHead = curr;
+        Node *prev = nullptr;
+        Node *nextNode = nullptr;
+        int count = 0;
+
+        // Reverse the nodes in the current group
+        while (curr != nullptr && count < k) {
+            nextNode = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = nextNode;
+            count++;
+        }
+
+        // If newHead is null, set it to the
+          // last node of the first group
+        if (newHead == nullptr) {
+            newHead = prev;
+        }
+
+        // Connect the previous group to the 
+          // current reversed group
+        if (tail != nullptr) {
+            tail->next = prev;
+        }
+
+        // Move tail to the end of the reversed group
+        tail = groupHead;
+    }
+
+    return newHead;
+}
+
+};
