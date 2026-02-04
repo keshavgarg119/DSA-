968c611eafb09b6977156cc7a177f53dedee5db4
keshavgarg119 <kgarg2_be23@thapar.edu>
2026-02-04 18:09:30 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Print Longest Increasing Subsequence/print-longest-increasing-subsequence.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    vector<int> getLIS(vector<int>& arr) {
+        int n = arr.size();
+
+        vector<int> dp(n, 1);
+        vector<int> hash(n);
+
+        int maxi = 1;
+        int lastIndex = 0;
+
+        for (int i = 0; i < n; i++) {
+            hash[i] = i;
+            for (int prev = 0; prev < i; prev++) {
+                if (arr[prev] < arr[i] && dp[prev] + 1 > dp[i]) {
+                    dp[i] = dp[prev] + 1;
+                    hash[i] = prev;
+                }
+            }
+            if (dp[i] > maxi) {
+                maxi = dp[i];
+                lastIndex = i;
+            }
+        }
+
+        // Reconstruct LIS
+        vector<int> lis;
+        lis.push_back(arr[lastIndex]);
+
+        while (hash[lastIndex] != lastIndex) {
+            lastIndex = hash[lastIndex];
+            lis.push_back(arr[lastIndex]);
+        }
+
+        reverse(lis.begin(), lis.end());
+        return lis;
+    }
+};
