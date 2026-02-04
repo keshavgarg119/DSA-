5d1ae7c63760ca39d9c6b297f3a962243d27dbcf
keshavgarg119 <kgarg2_be23@thapar.edu>
2026-02-04 17:37:31 +0530

    Time: 262 ms (35.65%) | Memory: 296.4 MB (26.33%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int f(int ind, int prev, vector<int>&arr, int n,  vector<vector<int>>&dp) {
+    /*int f(int ind, int prev, vector<int>&arr, int n,  vector<vector<int>>&dp) {
         if(ind==n) return 0;
         if(dp[ind][prev+1]!=-1) return dp[ind][prev+1];
 
@@ -10,10 +10,23 @@ public:
         }
 
         return dp[ind][prev+1] = length;
-    }
+    }*/
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
-        vector<vector<int>>dp(n, vector<int>(n+1,-1));
-        return f(0,-1,nums,n,dp);
+        vector<vector<int>>dp(n+1, vector<int>(n+1,0));
+
+
+        for(int ind=n-1; ind>=0; ind--) {
+            for(int prev=ind-1; prev>=-1; prev--) { 
+
+                int length = dp[ind+1][prev+1];
+                if(prev==-1 || nums[ind] > nums[prev]) {
+                    length = max(length, 1+dp[ind+1][ind+1]);
+                }
+
+            dp[ind][prev+1] = length;
+            }
+        }
+        return dp[0][-1+1];
     }
 };
