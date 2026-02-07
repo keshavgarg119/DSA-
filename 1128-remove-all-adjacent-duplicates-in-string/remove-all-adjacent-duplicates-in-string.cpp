3f806e14b697669831f74c042e203fd94231c33c
keshavgarg119 <kgarg2_be23@thapar.edu>
2026-02-07 21:31:47 +0530

    Time: 4 ms (67.82%) | Memory: 13.7 MB (66.82%) - LeetSync

--- 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.cpp
+++ 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.cpp
@@ -1,25 +1,16 @@
 class Solution {
 public:
     string removeDuplicates(string s) {
-        stack<char> st;
-        int n = s.size();
+        string st = "";
         
-        for(char c: s) {
-            if(!st.empty() && st.top()==c) {
-                st.pop();
-            }
-            else {
-                st.push(c);
+        for(char c : s) {
+            if(!st.empty() && st.back() == c) {
+                st.pop_back();   
+            } else {
+                st.push_back(c);
             }
         }
-
-        string ans = "";
-        while(!st.empty()) {
-            ans.push_back(st.top());
-            st.pop();
-        }
-
-        reverse(ans.begin(), ans.end());
-        return ans;
+        
+        return st;
     }
 };
