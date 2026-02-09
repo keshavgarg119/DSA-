45897ad3aeabe2b7adb67c34dccd0fa4e3eef088
keshavgarg119 <kgarg2_be23@thapar.edu>
2026-02-09 17:48:14 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Frog Jump/frog-jump.cpp
+++ Difficulty: Medium/Frog Jump/frog-jump.cpp
@@ -1,6 +1,6 @@
 class Solution {
   public:
-    int f(int ind, vector<int>& height,vector<int>& dp) {
+    /*int f(int ind, vector<int>& height,vector<int>& dp) {
         if(ind==0) return 0;
         if(dp[ind]!=-1) return dp[ind];
         
@@ -13,12 +13,29 @@ class Solution {
         
         return dp[ind] = min(jumpOne, jumpTwo);
         
-    }
+    }*/
+    // using tabulation
     int minCost(vector<int>& height) {
         // Code here
         int n = height.size();
-        vector<int>dp(n+1,-1);
+        //vector<int>dp(n,0);
+        int prev1 = 0;
+        int prev2 = 0;
         
-        return f(n-1,height,dp);
+        for(int ind=1; ind<n; ind++) {
+            int jumpOne = prev1 + abs(height[ind] - height[ind-1]); 
+            int jumpTwo = INT_MAX;
+            
+            if(ind>1) {
+                jumpTwo = prev2 + abs(height[ind] - height[ind-2]);
+            }
+            
+            int cur = min(jumpOne, jumpTwo);
+            
+            prev2 = prev1;
+            prev1 = cur;
+        }
+        
+        return prev1;
     }
 };
