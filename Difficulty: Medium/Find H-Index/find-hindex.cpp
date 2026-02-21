fd51b4f6a85517704b39d008fdab4dca0648f072
keshavgarg119 <kgarg2_be23@thapar.edu>
2026-02-21 21:25:38 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Find H-Index/find-hindex.cpp
@@ -0,0 +1,18 @@
+class Solution {
+  public:
+    int hIndex(vector<int>& citations) {
+        // code here
+        sort(citations.begin(), citations.end());
+        int maxi = 0;
+        
+        int n = citations.size();
+        for(int i=0; i<n; i++) {
+            if(citations[i] >= n-i) {
+                return n-i;
+            }
+        }
+        
+        return 0;
+        
+    }
+};
